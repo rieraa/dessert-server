4ca83b041997655ee4d07ca99a16a4d9ecccc35c
rieraa <ameee9@163.com>
2023-12-25 15:34:02 +0800

    ✨ JWT工具日志记录

--- desserts-user-server/src/main/java/com/example/desserts/utils/JwtTokenUtils.java
+++ desserts-user-server/src/main/java/com/example/desserts/utils/JwtTokenUtils.java
@@ -6,16 +6,17 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.desserts.entity.User;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
+@Slf4j
 public class JwtTokenUtils {
-    // token私钥
+    // Token私钥
     private static final String TOKEN_SECRET = "secret";
-    // token过期时长30分钟
-    private static final long EXPIRE_TIME = 1800L; // 单位为秒
+    // Token过期时长，单位为秒
+    private static final long TOKEN_EXPIRE_TIME_SECONDS = 1800L;
 
     /**
      * 生成用户token,设置token超时时间
@@ -23,7 +24,7 @@ public class JwtTokenUtils {
      * @return 生成的token
      */
     public static String createToken(User user) {
-        Date expireDate = new Date(System.currentTimeMillis() + EXPIRE_TIME * 1000);
+        Date expireDate = new Date(System.currentTimeMillis() + TOKEN_EXPIRE_TIME_SECONDS * 1000);
         // 创建 Token 的 payload
         Map<String, Object> map = new HashMap<>();
         map.put("alg", "HS256");
@@ -31,11 +32,10 @@ public class JwtTokenUtils {
         // 生成 JWT Token
         String token = JWT.create()
                 .withHeader(map)// 添加头部
-
                 .withClaim("userId", user.getUserId())
-                .withExpiresAt(expireDate) //超时设置,设置过期的日期
-                .withIssuedAt(new Date()) //签发时间
-                .sign(Algorithm.HMAC256(TOKEN_SECRET)); //SECRET加密
+                .withExpiresAt(expireDate) // 超时设置,设置过期的日期
+                .withIssuedAt(new Date()) // 签发时间
+                .sign(Algorithm.HMAC256(TOKEN_SECRET)); // SECRET加密
         return token;
     }
 
@@ -47,19 +47,19 @@ public class JwtTokenUtils {
      */
     public static Map<String, Claim> verifyToken(String token) {
         DecodedJWT jwt = null;
-        token = token.replace("Bearer ", ""); // 这里要去掉Bearer ，postman默认会加这个，导致匹配不上
+        // 去掉Bearer前缀
+        token = token.replaceFirst("Bearer\\s+", "").trim();
         try {
-            //设置签名的加密算法：HMAC256
+            // 设置签名的加密算法：HMAC256
             Algorithm algorithm = Algorithm.HMAC256(TOKEN_SECRET); // 私钥需要一致
             JWTVerifier verifier = JWT.require(algorithm).build();
             jwt = verifier.verify(token);
         } catch (Exception e) {
-//      logger.error(e.getMessage()); // 还没安装log4j等日志依赖，先注释
-//      logger.error("token解码异常");
-            //解码异常则抛出异常
+            // 记录异常信息
+            log.info("Token解码异常: " + e.getMessage());
+            // 解码异常则抛出异常
             return null;
         }
-
 
         return jwt.getClaims();
     }
