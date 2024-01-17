cc611b9378925a9d8c8f005dbeada24e42386882
rieraa <ameee9@163.com>
2024-01-17 16:13:28 +0800

    ✨ 过滤器使用通用响应类返回

--- desserts-user-server/src/main/java/com/example/desserts/config/JwtFilter.java
+++ desserts-user-server/src/main/java/com/example/desserts/config/JwtFilter.java
@@ -1,22 +1,20 @@
 package com.example.desserts.config;
 
 import com.auth0.jwt.interfaces.Claim;
+import com.example.desserts.domain.ResponseResult;
+import com.example.desserts.enums.BusinessCode;
 import com.example.desserts.utils.JwtTokenUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Component
 @WebFilter(filterName = "JwtFilter", urlPatterns = "/*")
@@ -24,6 +22,7 @@ public class JwtFilter implements Filter {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
 
+    // 设置允许直接访问的路径
     private static final Set<String> ALLOWED_PATHS = new HashSet<>(Arrays.asList("/user/login", "/user/register", "/dessert/getAllDessert"));
 
     private static final String NO_TOKEN_MESSAGE = "用户未登录";
@@ -31,7 +30,7 @@ public class JwtFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        // Initialization logic if needed
+        System.out.println("请求过滤初始化");
     }
 
     @Override
@@ -39,18 +38,25 @@ public class JwtFilter implements Filter {
         final HttpServletRequest request = (HttpServletRequest) req;
         final HttpServletResponse response = (HttpServletResponse) res;
 
+        // 设置响应字符编码为UTF-8
         response.setCharacterEncoding("UTF-8");
-        final String token = request.getHeader("Authorization");
 
+        // 获取请求头中的token
+        final String token = request.getHeader("Authorization");
+        System.out.println("🚀 ~ file:JwtFilter.java method:doFilter line:48 -----token:" + token);
+        
+
+        // 获取请求路径
         String requestPath = request.getRequestURI();
 
-        if (ALLOWED_PATHS.stream().anyMatch(requestPath::contains)) {
+        // 如果请求路径在允许直接访问的路径列表中，则直接放行
+        if (ALLOWED_PATHS.stream().anyMatch(allowedPath -> requestPath.contains(allowedPath))) {
             chain.doFilter(request, response);
             return;
         }
 
+        // 如果请求方法为OPTIONS，表示为预检请求，直接放行
         if ("OPTIONS".equals(request.getMethod())) {
-            // 是否是 OPTIONS 请求
             response.setStatus(HttpServletResponse.SC_OK);
             chain.doFilter(request, response);
         } else {
@@ -59,28 +65,49 @@ public class JwtFilter implements Filter {
         }
     }
 
+    /**
+     * 鉴权逻辑
+     *
+     * @param request
+     * @param response
+     * @param token
+     * @param chain
+     * @throws IOException
+     * @throws ServletException
+     */
     private void performAuthorization(HttpServletRequest request, HttpServletResponse response,
                                       String token, FilterChain chain) throws IOException, ServletException {
-        if (token == null) {
-            response.getWriter().write(NO_TOKEN_MESSAGE);
+        // 如果token为空，返回用户未登录信息
+        if (Objects.equals(token, "")) {
+            String jsonResponse = new ObjectMapper().writeValueAsString(ResponseResult.errorResult(BusinessCode.USER_NOT_LOGIN));
+            response.setContentType("application/json");
+            response.getWriter().write(jsonResponse);
             return;
         }
+
+        // 验证token的合法性，并获取用户数据
         Map<String, Claim> userData = JwtTokenUtils.verifyToken(token);
+
+        // 如果用户数据为空，返回token失效信息
         if (userData == null) {
-            response.getWriter().write(ILLEGAL_TOKEN_MESSAGE);
+            String jsonResponse = new ObjectMapper().writeValueAsString(ResponseResult.errorResult(BusinessCode.TOKEN_EXPIRED));
+            response.setContentType("application/json");
+            response.getWriter().write(jsonResponse);
             return;
         }
 
         // 获取用户名
         int userId = userData.get("userId").asInt();
-        // 设置用户名
+
+        // 设置用户名到请求属性中
         request.setAttribute("userId", userId);
-        // 过滤成功
+
+        // 过滤成功，继续处理请求
         chain.doFilter(request, response);
     }
 
-    @Override
-    public void destroy() {
-        // Destruction logic if needed
+@Override
+public void destroy() {
+        // 销毁逻辑（如果有需要）
     }
 }
