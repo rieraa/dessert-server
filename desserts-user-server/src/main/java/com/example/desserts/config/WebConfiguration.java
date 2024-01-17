61cd62fa3dda0f3b79da0e2019b32c987ee5c407
rieraa <ameee9@163.com>
2024-01-17 16:14:59 +0800

    💡 注释

--- desserts-user-server/src/main/java/com/example/desserts/config/WebConfiguration.java
+++ desserts-user-server/src/main/java/com/example/desserts/config/WebConfiguration.java
@@ -1,5 +1,4 @@
 package com.example.desserts.config;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
@@ -7,8 +6,10 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Web配置类，用于配置拦截器等Web相关设置。
+ */
 @Configuration
-
 public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
