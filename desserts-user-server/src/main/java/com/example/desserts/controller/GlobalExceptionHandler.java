d8178ca283874cff99b427b64a3f75c7798d57d9
rieraa <ameee9@163.com>
2024-01-19 21:16:05 +0800

    ♻️ 全局异常处理优化

--- desserts-user-server/src/main/java/com/example/desserts/controller/GlobalExceptionHandler.java
+++ desserts-user-server/src/main/java/com/example/desserts/controller/GlobalExceptionHandler.java
@@ -2,21 +2,20 @@ package com.example.desserts.controller;
 
 import com.example.desserts.domain.ResponseResult;
 import com.example.desserts.exception.BusinessException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@ControllerAdvice
+@RestController
 public class GlobalExceptionHandler {
+    //    异常处理方法的注解，当捕获到BusinessException类型的异常时，将执行这个方法
     @ExceptionHandler(BusinessException.class)
-    @ResponseBody
     public ResponseResult<Object> handleBusinessException(BusinessException ex) {
         return new ResponseResult<>(ex.getCode(), ex.getDescription(), null);
     }
 
     @ExceptionHandler(Exception.class)
-    @ResponseBody
     public ResponseResult<Object> handleException(Exception ex) {
-        return new ResponseResult<>(500, "Internal Server Error", null);
+        return new ResponseResult<>(500, "系统异常，请联系管理员", null);
     }
 }
