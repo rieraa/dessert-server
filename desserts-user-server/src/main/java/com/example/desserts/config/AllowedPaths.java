312b29485270b5acf78dfbbbbb06383fe464c6c9
rieraa <ameee9@163.com>
2024-01-30 16:21:43 +0800

    ✨ 新增直接访问路由

---
+++ desserts-user-server/src/main/java/com/example/desserts/config/AllowedPaths.java
@@ -0,0 +1,16 @@
+package com.example.desserts.config;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class AllowedPaths {
+
+    public static final Set<String> ALLOWED_PATHS = new HashSet<>(Arrays.asList(
+            "/user/login",
+            "/user/register",
+            "/dessert/getAllDessert",
+            "/dessert/getDessertById"
+    ));
+
+}
