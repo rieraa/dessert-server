02245a79ed6b96e7a0b521b15666ff2458438a1a
HCX <1874640833@qq.com>
2024-02-16 00:15:54 +0800

    feat: t-desgin

--- desserts-user-front/src/main.js
+++ desserts-user-front/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import TDesign from 'tdesign-vue-next';
+// 引入组件库的少量全局样式变量
+import 'tdesign-vue-next/es/style/index.css';
 
-createApp(App).mount('#app')
+
+import App from './App.vue'
+const app = createApp(App);
+
+app.use(TDesign);
+app.mount('#app')
