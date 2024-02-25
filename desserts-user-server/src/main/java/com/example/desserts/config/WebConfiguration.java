a02dfd6410fe84493265753ab192d8ccdf879f87
rieraa <ameee9@163.com>
2024-02-25 18:14:50 +0800

    ✨ 跨域配置

--- desserts-user-server/src/main/java/com/example/desserts/config/WebConfiguration.java
+++ desserts-user-server/src/main/java/com/example/desserts/config/WebConfiguration.java
@@ -1,10 +1,13 @@
 package com.example.desserts.config;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 /**
  * Web配置类，用于配置拦截器等Web相关设置。
@@ -21,4 +24,14 @@ public class WebConfiguration implements WebMvcConfigurer {
             }
         });
     }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 所有接口
+                .allowCredentials(true) // 是否发送 Cookie
+                .allowedOriginPatterns("*") // 支持域
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 支持方法
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+
 }
